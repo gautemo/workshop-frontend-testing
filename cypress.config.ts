2a5b9dd9a43cdeff1031fdc34ebd44ff8005b5bc
Gaute Meek Olsen <gaute.meek@gmail.com>
2022-06-29 00:34:35 +0200

    test in ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:3000',
+		video: false,
 	},
-
 	component: {
+		video: false,
 		devServer: {
 			framework: 'react',
 			bundler: 'vite',
--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -1,8 +1,7 @@
 import App from '../../src/App'
-import { mount } from 'cypress/react'
 
 describe('App.cy.ts', () => {
 	it('playground', () => {
-		mount(<App />)
+		cy.mount(<App />)
 	})
 })
